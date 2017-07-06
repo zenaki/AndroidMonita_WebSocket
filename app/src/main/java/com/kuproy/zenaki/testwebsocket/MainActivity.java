73d26f92422b5ebdf30f9bd40016ce6b8c09583a
Dendy Gema <Zenaki Kurniawan>
2017-07-06 10:28:46 +0700

    Updating Table when get Messages

--- app/src/main/java/com/kuproy/zenaki/testwebsocket/MainActivity.java
+++ app/src/main/java/com/kuproy/zenaki/testwebsocket/MainActivity.java
@@ -2,9 +2,20 @@ package com.kuproy.zenaki.testwebsocket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -13,6 +24,8 @@ import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import okio.ByteString;
 
+import static java.security.AccessController.getContext;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText server_address;
@@ -20,10 +33,17 @@ public class MainActivity extends AppCompatActivity {
     private EditText data_viewer;
     private EditText message;
     private Button button_send;
+    private TableLayout tb_data;
     private OkHttpClient client;
 
     private WebSocket ws;
     private static final int NORMAL_CLOSURE_STATUS = 1000;
+
+    private String epochtime[];
+    private String nama_tu[];
+    private String serial_number[];
+    private String titik_ukur[];
+    private String value[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         data_viewer = (EditText) findViewById(R.id.data_viewer);
         message = (EditText) findViewById(R.id.message);
         button_send = (Button) findViewById(R.id.button_send);
+        tb_data = (TableLayout) findViewById(R.id.tb_data);
 
         client = new OkHttpClient();
 
@@ -108,6 +129,36 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onMessage(WebSocket webSocket, String text) {
+            try {
+                JSONObject data = new JSONObject(text);
+                JSONArray array = data.getJSONArray("monita");
+
+                epochtime = new String[array.length()];
+                nama_tu = new String[array.length()];
+                serial_number = new String[array.length()];
+                titik_ukur = new String[array.length()];
+                value = new String[array.length()];
+
+                for (int i = 0; i < array.length(); i++) {
+                    try {
+                        JSONObject obj = array.getJSONObject(i);
+
+                        epochtime[i] = obj.getString("epochtime").toString();
+                        nama_tu[i] = obj.getString("nama_tu").toString();
+                        serial_number[i] = obj.getString("serial_number").toString();
+                        titik_ukur[i] = obj.getString("titik_ukur").toString();
+                        value[i] = obj.getString("value").toString();
+                    } catch (JSONException e) {
+                        // Oops
+                    }
+                }
+
+                setTable(array.length());
+            } catch (JSONException e) {
+                Log.e("TestWebSocket", "unexpected JSON exception", e);
+            }
+//            Log.d("TestWebSocket",data);
+//            Log.d("TestWebSocket",text);
             setMessage(text);
         }
 
@@ -126,5 +177,89 @@ public class MainActivity extends AppCompatActivity {
         public void onFailure(WebSocket webSocket, Throwable t, Response response) {
             setMessage("Error : " + t.getMessage());
         }
+    }
+
+    private void setTable(final int ln) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (tb_data.getChildCount() >= ln) {
+                    for (int i = 0; i < ln; i++) {
+                        TableRow row = (TableRow) tb_data.getChildAt(i+1);
+
+                        try {
+                            TextView number_view = (TextView) row.getChildAt(0);
+                            number_view.setText(String.valueOf(i+1));
+                            TextView epochtime_view = (TextView) row.getChildAt(1);
+                            epochtime_view.setText(epochtime[i]);
+                            TextView nama_tu_view = (TextView) row.getChildAt(2);
+                            nama_tu_view.setText(nama_tu[i]);
+                            TextView serial_number_view = (TextView) row.getChildAt(3);
+                            serial_number_view.setText(serial_number[i]);
+                            TextView titik_ukur_view = (TextView) row.getChildAt(4);
+                            titik_ukur_view.setText(titik_ukur[i]);
+                            TextView value_view = (TextView) row.getChildAt(5);
+                            value_view.setText(value[i]);
+                        } catch (Exception e) {
+                            TextView number_view = (TextView) row.getChildAt(0);
+                            number_view.setText("");
+                            TextView epochtime_view = (TextView) row.getChildAt(1);
+                            epochtime_view.setText("");
+                            TextView nama_tu_view = (TextView) row.getChildAt(2);
+                            nama_tu_view.setText("");
+                            TextView serial_number_view = (TextView) row.getChildAt(3);
+                            serial_number_view.setText("");
+                            TextView titik_ukur_view = (TextView) row.getChildAt(4);
+                            titik_ukur_view.setText("");
+                            TextView value_view = (TextView) row.getChildAt(5);
+                            value_view.setText("");
+                        }
+                    }
+                } else {
+                    if (tb_data.getChildCount() > 1) {
+                        tb_data.removeViews(1, ln);
+                    }
+                    for (int i = 0; i < ln; i++) {
+                        TableRow row = new TableRow(MainActivity.this);
+                        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+                        row.setLayoutParams(lp);
+                        row.setPadding(dpToPx(5), dpToPx(5), dpToPx(100), dpToPx(5));
+                        row.setId(i+1);
+
+                        TextView number_view = new TextView(MainActivity.this);
+                        number_view.setText(String.valueOf(i+1));
+                        TextView epochtime_view = new TextView(MainActivity.this);
+                        epochtime_view.setText(epochtime[i]);
+                        TextView nama_tu_view = new TextView(MainActivity.this);
+                        nama_tu_view.setText(nama_tu[i]);
+                        TextView serial_number_view = new TextView(MainActivity.this);
+                        serial_number_view.setText(serial_number[i]);
+                        TextView titik_ukur_view = new TextView(MainActivity.this);
+                        titik_ukur_view.setText(titik_ukur[i]);
+                        TextView value_view = new TextView(MainActivity.this);
+                        value_view.setText(value[i]);
+
+                        row.addView(number_view);
+                        row.addView(epochtime_view);
+                        row.addView(nama_tu_view);
+                        row.addView(serial_number_view);
+                        row.addView(titik_ukur_view);
+                        row.addView(value_view);
+
+                        tb_data.addView(row);
+                    }
+                }
+            }
+        });
+    }
+
+    public int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    public int pxToDp(int px) {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 }
